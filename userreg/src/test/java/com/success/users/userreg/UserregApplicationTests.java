e216fcc9d47026520c79dca6b3dda770ecb9d3e5
amantyagi9999 <amantyagi431@gmail.com>
2024-10-01 19:38:09 +0530

    User app added

---
+++ userreg/src/test/java/com/success/users/userreg/UserregApplicationTests.java
@@ -0,0 +1,13 @@
+package com.success.users.userreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserregApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
